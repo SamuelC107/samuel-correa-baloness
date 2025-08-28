3c805c1da13c25415c737225dfc0ff6e08bc788c
SamuelC107 <samuelcorrea1116986@correo.itm.edu.co>
2025-08-28 09:55:23 -0500

    Clases hechas Desarrollo

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,38 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+public class Tipos
+{
+    public int Id;
+    public string? Descripcion;
+    public List<Balones>? Balones;
+}
+
+public class Marcas
+{
+    public int Id;
+    public string? Nombre;
+    public List<Balones>? Balones;
+}
+
+public class Bodegas
+{
+    public int Id;
+    public string? Nombre;
+    public string? Direccion;
+    public int Capacidad;
+    public List<Balones>? balones;
+}
+
+public class Balones
+{
+    public int Id;
+    public string? Nombre;
+    public decimal Peso;
+    public bool Disponible;
+    public string? Color;
+    public double Precio;
+    public Tipos? Tipo;
+    public Marcas? Marca;
+    public Bodegas? Bodega;
+}
+
